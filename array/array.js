8c663e04c104d00443eac8ffbcf46fa9af52ef0e
Axel Espinosa <axeldavid45@gmail.com>
2021-01-11 19:43:38 -0600

    feat: pop method added to array class

--- array/array.js
+++ array/array.js
@@ -1,5 +1,5 @@
 class MyArray {
-  constructor() {
+  constructor () {
     this.lenght = 0
     this.data = {}
   }
@@ -7,9 +7,22 @@ class MyArray {
   get (index) {
     return this.data[index]
   }
-  
-  push(item) {
+
+  push (item) {
     this.data[this.lenght++] = item
     return this.data
   }
+
+  pop () {
+    const keys = Object.keys(this.data)
+    if (keys.length > 0) {
+      delete this.data[keys[keys.length - 1]]
+    }
+    return this.data
+  }
 }
+
+const myArray = new MyArray()
+console.log(myArray.push(1))
+console.log(myArray.pop())
+console.log(myArray.pop())
