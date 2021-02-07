afac8cd1d797e3526699780e4bc3ed1d708f5638
Axel Espinosa <axeldavid45@gmail.com>
2021-02-07 16:00:40 -0600

    feat(trees): added search  method

--- src/trees/tree.js
+++ src/trees/tree.js
@@ -16,18 +16,22 @@ class BinarySearchTree {
     const node = new Node(value)
     if (this.root === null) {
       this.root = node
+      this.depth++
+      return this
     } else {
       let currentNode = this.root
       while (true) {
         if (value < currentNode.value) {
           if (!currentNode.left) {
             currentNode.left = node
+            this.depth++
             return this
           }
           currentNode = currentNode.left
         } else {
           if (!currentNode.rigth) {
             currentNode.rigth = node
+            this.depth++
             return this
           }
           currentNode = currentNode.rigth
@@ -35,7 +39,42 @@ class BinarySearchTree {
       }
     }
   }
+
+  search (value) {
+    if (value === this.root.value) {
+      return this.root
+    } else {
+      let currentNode = this.root
+      while (true) {
+        if (value < currentNode.value) {
+          currentNode = currentNode.left
+          if (currentNode) {
+            if (value === currentNode.value) {
+              return currentNode
+            }
+          } else {
+            return false
+          }
+        } else {
+          currentNode = currentNode.rigth
+          if (currentNode) {
+            if (value === currentNode.value) {
+              return currentNode
+            }
+          } else {
+            return false
+          }
+        }
+      }
+    }
+  }
 }
 
 const tree = new BinarySearchTree()
-tree.insert(10)
+tree.insert(101)
+tree.insert(90)
+tree.insert(80)
+tree.insert(92)
+tree.insert(105)
+tree.insert(200)
+tree.insert(300)
